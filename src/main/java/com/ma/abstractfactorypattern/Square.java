435cccbc384fd35f0996ac946e23b89ab23b09ed
machd <1224352076>
2021-01-04 17:21:54 +0800

    feat: 添加自己抽象工厂的理解

---
+++ src/main/java/com/ma/abstractfactorypattern/Square.java
@@ -0,0 +1,14 @@
+package com.ma.abstractfactorypattern;
+
+/**
+ * 请添加注释
+ *
+ * @author <a href="mailto:macd@zjport.gov.cn">macd</a>
+ * @version 3
+ * @since 3.0
+ */
+public class Square implements Shape {
+    public void draw() {
+        System.out.println("画一个正方形");
+    }
+}
